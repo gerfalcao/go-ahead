0e683f2460d3427cca8fd1e64ea8a84efc2f6d7e
Germanno Falcão <germannoiknow@gmail.com>
2024-06-09 11:12:43 -0300

    feat: show and update CRUD rules

--- handler/request.go
+++ handler/request.go
@@ -55,3 +55,19 @@ func InitializeHandler(){
 	logger = config.GetLogger("handler")
 	db = config.GetSQLite()
 }
+
+type UpdateOpeningRequest struct {
+	Role string			`json:"role"`
+	Company string		`json:"company"`
+	Location string		`json:"location"`
+	Remote *bool		`json:"remote"`
+	Link string			`json:"link"`
+	Salary int64		`json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	return fmt.Errorf("at least one field on request must be provided")
+}
--- handler/show.go
+++ handler/show.go
@@ -3,13 +3,24 @@ package handler
 import (
 	"net/http"
 
+	"github.com/gerfalcao/go-ahead.git/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	id := ctx.Query("id")
+	if id==""{
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error;err!=nil{
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
